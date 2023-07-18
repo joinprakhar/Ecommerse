9ad1eec3f618b87afd1191d63f01a04af68c41da
joinprakhar <pandeylive@gmail.com>
2023-07-18 12:40:00 +0530

    HomePage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Header from './components/Header'
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
-    <div className='container'>App</div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen/>
+        </Container>
+      </main>
+      <Footer/>
+    </>
   )
 }
 
