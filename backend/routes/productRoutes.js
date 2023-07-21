85c1e53f9ab12dc4071023fb3bb6152c9292c4da
joinprakhar <pandeylive@gmail.com>
2023-07-21 17:12:13 +0530

    review product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import { getProducts, getProductById, createProduct, updateProduct, deleteProduct } from '../controllers/productController.js';
+import { getProducts, getProductById, createProduct, updateProduct, deleteProduct, createProductReview } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js'
 import { createConnection } from 'mongoose';
 
@@ -9,5 +9,6 @@ router.route('/:id')
     .get(getProductById)
     .put(protect, admin, updateProduct)
     .delete(protect, admin, deleteProduct);
+router.route('/:id/reviews').post(protect, createProductReview)
 
 export default router;
