c416f0974d1b75031072be0ad19eaa22c98e70ff
joinprakhar <pandeylive@gmail.com>
2023-07-31 09:10:55 +0530

    bug fix

--- server/server.js
+++ server/server.js
@@ -46,10 +46,10 @@ app.get('/api/config/paypal',
 if (process.env.NODE_ENV === 'production') {
     const __dirname = path.resolve();
     app.use('/uploads', express.static('/var/data/uploads'));
-    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    app.use(express.static(path.join(__dirname, '/app/build')));
 
     app.get('*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'app', 'build', 'index.html'))
     );
 } else {
     const __dirname = path.resolve();
